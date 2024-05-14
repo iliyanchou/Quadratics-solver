26f3a546ae3309187e75bcdc68973185dbe0f7b8
Ilia Granchulov <166340452+iliyanchou@users.noreply.github.com>
2024-05-14 17:00:56 +0300

    quadratic.js

--- quadratic.js
+++ quadratic.js
@@ -54,4 +54,74 @@ function solve() {
     }
 }
 
+function graph() {
+    var a = document.getElementById('firstcoef').value;
+    var a = parseFloat(a);
+    if (a !== 0) {
+        function f(x) {
+            var a = document.getElementById('firstcoef').value;
+            var a = parseFloat(a);
+            var b = document.getElementById('secondcoef').value;
+            var b = parseFloat(b);
+            var c = document.getElementById('thirdcoef').value;
+            var c = parseFloat(c);
+            return a * x * x + b * x + c
+        }
+        var a = document.getElementById('firstcoef').value;
+        var a = parseFloat(a);
+        var b = document.getElementById('secondcoef').value;
+        var b = parseFloat(b);
+        var c = document.getElementById('thirdcoef').value;
+        var c = parseFloat(c);
+        const xValues = [];
+        const yValues = [];
+        var sumOfCoef = Math.abs(a + b + c);
+        if (sumOfCoef <= 1000) {
+            var sumOfCoef = sumOfCoef * 50;
+        }
+        else { }
+        if (sumOfCoef == 0) {
+            var sumOfCoef = 10;
+        }
+        else { }
+        if (sumOfCoef > 100000) {
+            var sumOfCoef = 10000;
+        }
+        console.log(sumOfCoef);
+        for (let x = -sumOfCoef; x <= sumOfCoef; x += 1) {
+            xValues.push(x);
+            yValues.push(f(x));
+        }
 
+        const yIntercept = document.getElementById('thirdcoef').value;
+
+
+
+        const trace = {
+            x: xValues,
+            y: yValues,
+            type: 'scatter',
+            mode: 'lines',
+            line: { shape: 'spline' },
+            name: 'f(x)'
+        };
+        const trace2 = {
+            x: [0],
+            y: [yIntercept],
+            type: 'scatter',
+            mode: 'markers',
+            marker: { color: 'red', size: 10 },
+            name: 'y-intercept'
+        };
+        const layout = {
+            const: a = document.getElementById('firstcoef').value,
+            const: b = document.getElementById('secondcoef').value,
+            const: c = document.getElementById('thirdcoef').value,
+            title: `Graph of f(x) = ${a}x\u00B2 + (${b})x + (${c})`,
+            xaxis: { title: 'x' },
+            yaxis: { title: 'f(x)' }
+        };
+        const data = [trace, trace2];
+        Plotly.newPlot('plot', data, layout);
+    }
+}
